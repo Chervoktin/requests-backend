ede2e1971bbf8cb1f57222e18699be818c413b06
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-15 22:57:47 +0300

    changed views.py

--- mysite/views.py
+++ mysite/views.py
@@ -3,15 +3,18 @@ from django import forms
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import ensure_csrf_cookie
+import json
 
 class SumForm(forms.Form):
     a = forms.DecimalField()
     b = forms.DecimalField()
 
-@ensure_csrf_cookie
+@csrf_exempt
 def sum(request):
     if request.method == 'POST':
         form = SumForm(request.POST)
         if form.is_valid():
             return HttpResponse(form.cleaned_data['a'] + form.cleaned_data['b'])
+        else:
+            return HttpResponse(json.dumps(form.errors))
  
