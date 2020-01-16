4bede2dae88d74ff886da11f3d0d4b18a836391d
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-01-16 17:24:42 +0300

    changed views.py

--- mysite/views.py
+++ mysite/views.py
@@ -3,11 +3,15 @@ from django import forms
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import ensure_csrf_cookie
+from mysite.models import Person
 import json
+from django.core import serializers
+
 
 class SumForm(forms.Form):
     a = forms.DecimalField()
     b = forms.DecimalField()
+
 
 @csrf_exempt
 def sum(request):
@@ -17,4 +21,8 @@ def sum(request):
             return HttpResponse(form.cleaned_data['a'] + form.cleaned_data['b'])
         else:
             return HttpResponse(json.dumps(form.errors))
- 
+
+def persons(request):
+    if request.method == 'GET':
+        data = serializers.serialize("json", Person.objects.raw("select * from mysite_person"))
+        return HttpResponse(data)
